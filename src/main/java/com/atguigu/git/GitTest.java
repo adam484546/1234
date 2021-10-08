56618381e339586709cab8a7e3185ae9dc328a5d
adam <guijin_liuer@163.com>
2021-10-08 20:30:35 +0800

    11111111111111111111111

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author guijin
+ * @date 2021/10/8
+ * description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
